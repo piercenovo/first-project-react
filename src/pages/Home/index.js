c7bd295e4a4ba550e216866a2d1473efcfc78b83
Pierce Novoa <piercenh17@gmail.com>
2020-09-25 18:50:35 -0500

    Agrego buscador

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,29 @@
-import React from 'react'
-import {Link} from 'wouter'
+import React, { useState } from 'react'
+import {Link, useLocation} from 'wouter'
 
-const GIFS = ["panda", "programador", "bobesponja", "muebles", "perros"]
+const GIFS = ["Pandas", "Perros", "Bob Esponja", "Muebles", "Gatos"]
 
 export default function Home() {
+
+    const [keyword, setKeyword] = useState('')
+    const [path, pushLocation] = useLocation()
+
+    const handleSubmit = evt => {
+        //Navegar a otra ruta
+        pushLocation(`/search/${keyword}`)
+    }
+
+    const handleChange = evt => {
+        //Hace el cambio
+        setKeyword(evt.target.value)
+    }
+
     return (
         <>
+        <form onSubmit={handleSubmit}>
+            <input placeholder="Busca el gif aquí.." onChange={handleChange} type="text" value={keyword} />
+        </form>
         <h3>Disfruta los gifs</h3>
-
         <ul>
             {GIFS.map(enjoyGif => (
                 <li key={enjoyGif}>
